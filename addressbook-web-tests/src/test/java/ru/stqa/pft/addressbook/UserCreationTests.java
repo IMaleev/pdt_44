a013476e3c7e5196b971c1afaeb9a9b77d819076
Ilya Maleev <imaleev@natera.com>
2017-08-18 12:47:11 +0300

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге Переименовал тестовый метод

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserCreationTests.java
@@ -32,7 +32,7 @@ public class UserCreationTests {
     }
 
     @Test
-    public void UserCreationTests() {
+    public void testUserCreation() {
         initUserCreation();
         fillNewUserForm(new UserData("First Name", "Middle Name", "Last Name", "Nick Name", "Title", "Company", "Address", "111", "222", "333", "444", "email1@gmail.com", "email2@gmail.com", "email3@gmail.com", "www.google.com", "Address2", "55555", "Notes"));
         submitNewUserForm();
