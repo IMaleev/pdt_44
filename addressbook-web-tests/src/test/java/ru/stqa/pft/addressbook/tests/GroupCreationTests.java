a86efab08a6c4544fe0aa4d83c761fb484f643f8
Ilya Maleev <imaleev@natera.com>
2017-08-24 23:48:51 +0300

    Обеспечение выполнения предусловий для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -10,13 +10,7 @@ public class GroupCreationTests extends TestBase {
         app.getNavigationHelper()
            .gotoGroupPage();
         app.getGroupHelper()
-           .initGroupCreation();
-        app.getGroupHelper()
-           .fillGroupForm(new GroupData("test1", "test2", "test3"));
-        app.getGroupHelper()
-           .submitGroupCreation();
-        app.getGroupHelper()
-           .returnToGroupPage();
+           .createGroup(new GroupData("test1", "test2", "test3"));
     }
 
 }
